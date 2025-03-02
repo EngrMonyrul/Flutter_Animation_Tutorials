7058a9a2e8a2e2004e9bd0ca1c547063e7524deb
Monirul <monirul@primetechbd.com>
2025-03-02 23:14:35 +0600

    out of folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:animation_tutorials/animations/animated_align_example.dart';
+import 'package:animation_tutorials/animated_align_example.dart';
 import 'package:flutter/material.dart';
 
 void main() {
